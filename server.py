cdde5bfc0e2c41cc2f24fd6ff97702f54c98e1b7
Shahper-Zia <shahper.zia.iitr@gmail.com>
2025-03-15 13:42:21 +0530

    Update server.py
    added port and host

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+import os
 from chainlit.cli import run_chainlit
 
 def start():
+    os.environ["CHAINLIT_HOST"] = "0.0.0.0"  # Listen on all interfaces
+    os.environ["CHAINLIT_PORT"] = "8000"  # Listen on port 8000
     run_chainlit("app2.py")
 
 if __name__ == "__main__":
